7f6db8fc854499dd4c08527bbf61820d0ea53275
Josef1923 <schererjos01@gmail.com>
2025-03-29 21:49:12 +0100

    Update navigation clavier

--- src/Dropdown.jsx
+++ src/Dropdown.jsx
@@ -1,50 +1,79 @@
 import { useState } from "react";
 import "./Dropdown.css";
 
-/**
- * Composant Dropdown
- * @param {Array} options - Liste des options à afficher dans le dropdown au format {label: string, value: any}
- * @param {Function} onChange - Fonction appelée quand une option est sélectionnée, elle prend en paramètre la value de l'option sélectionnée * 
- */
+function Dropdown({ options, onChange, icon, disabled = false }) {
 
-function Dropdown({ options, onChange, icon, disabled = false}) {
+    const [isOpen, setIsOpen] = useState(false); // pour savoir si le menu est ouvert ou non
+    const [selectedOption, setSelectedOption] = useState(options[0] || null); // pour mettre à jour l'option sélectionnée dns la liste
 
-    const [isOpen, setIsOpen] = useState(false);
-    const [selectedOption, setSelectedOption] = useState(options[0] || null);
+    // const en vu de selcetionner l'option focusée par le clavier
+    const [focusedList, setFocusedList] = useState(); // pour savoir quelle option est focusée par le clavier(option keyboard)
 
+    // Fonction pour gérer le clic sur le bouton du menu déroulant
     const Toggle = () => {
-        //Si le dropdown est désactivé, on ne fait rien
-        if (disabled) {
-            return;
-        }
+        if (disabled) return;
         setIsOpen(!isOpen);
-    }
+    };
 
-    //Fonction appelée lorsqu'on clique sur une option
+    // Fonction pour gérer la sélection d'une option
     const onSelect = (option) => {
-        //mise à jour de l'option sélectionnée
         setSelectedOption(option);
-        //fermeture du dropdown
         setIsOpen(false);
-        //Appel de la fonction onChange passée en props
-        if (onChange) {
+        if (onChange) { //on vérifie jsute au cas où onChange n'est pas défini pour éviter les erreurs
             onChange(option.value);
         }
-    }
+
+    };
+
+    const handleKeyDown = (e) => {
+        if (disabled) return;
+
+        if (e.key === "ArrowDown" || e.key === "ArrowRight") {
+            //Si prev n'est pas le dernier élément de la liste, on l'incrémente de 1 sinon on le met à la dernière option de la liste
+            setFocusedList(prev => (prev < options.length - 1 ? prev + 1 : options.length - 1));  
+        }
+            // Si prev n'est pas le premier élément de la liste, on le décrémente de 1 sinon on le met à 0
+        if (e.key === "ArrowUp" || e.key === "ArrowLeft") {
+            setFocusedList(prev => (prev > 0 ? prev - 1 : 0));
+        }
+
+        if (e.key === "Enter" || e.key === " ") {         
+            e.preventDefault()
+            if (!isOpen) { // déclenche si le menu est fermé
+                setIsOpen(true); // ouvre le menu
+                setFocusedList(0); // met le focus sur la première option
+            }
+            else if (focusedList >= 0) { 
+                onSelect(options[focusedList]);//permet de sélectionner l'option focusée par le clavier
+            }
+        }
+ 
+        if (e.key === "Escape") {
+            setIsOpen(false);
+        }
+    };
 
     return (
         <div className="jsWrapper">
-            <div className="jsDropdown">
-                <button className={`jsDropdownButton ${isOpen ? "open" : ""}`} 
-                onClick={Toggle}
-                disabled={disabled}>
+            <div className="jsDropdown"
+                onKeyDown={handleKeyDown}
+                tabIndex={0} // pour ne pas perdre le focus du menu déroulant
+            >
+                <button className={`jsDropdownButton ${isOpen ? "open" : ""}`}
+                    onClick={Toggle}
+                    disabled={disabled} //pour garder l'option de la version jquery
+                >
                     {selectedOption.label || "Sélectionner une option"}
                     {icon && <span className="jsDropdownIcon">{icon}</span>}
                 </button>
                 {isOpen && (
                     <ul className="jsDropdownList">
                         {options.map((option, index) => (
-                            <li key={index} onClick={() => onSelect(option)}>
+                            <li
+                                key={index}
+                                onClick={() => onSelect(option)}                               
+                                className={index === focusedList ? "focused" : ""}                                
+                            >
                                 {option.label}
                             </li>
                         ))}
