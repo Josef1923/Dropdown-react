5b26b1dd2bdb699f4083a91594ee4f4f6bc0ad2c
Josef1923 <schererjos01@gmail.com>
2025-04-05 12:16:07 +0200

    Ajout options navigation clavier manquantes

--- src/Dropdown.jsx
+++ src/Dropdown.jsx
@@ -15,6 +15,11 @@ function Dropdown({ options, onChange, icon, disabled = false }) {
     // Fonction pour gérer le clic sur le bouton du menu déroulant
     const Toggle = () => {
         if (disabled) return;
+
+        if (!isOpen) {
+            const index = options.findIndex(option => option === selectedOption);
+            setFocusedList(index);
+        }
         setIsOpen(!isOpen);
     };
 
@@ -23,29 +28,39 @@ function Dropdown({ options, onChange, icon, disabled = false }) {
         setSelectedOption(option);
         setIsOpen(false);
         if (onChange) { //on vérifie jsute au cas où onChange n'est pas défini pour éviter les erreurs
-                onChange(option.value);
-            }
+            onChange(option.value);
+        }
 
     };
 
     const handleKeyDown = (e) => {
         if (disabled) return;
 
+        if ((e.key === "ArrowDown" || e.key === "ArrowUp") && e.altKey) {
+            e.preventDefault();
+            Toggle();
+            if (!isOpen) {
+                setFocusedList(0);
+            }
+            return;
+
+        }
+
         if (!isOpen) {
             // Si le menu n'est pas ouvert, on gère les touches fléchées pour changer l'option sélectionnée
             // on recupère l'index de l'option sélectionnée dans le tableau d'options
             const currentIndex = options.findIndex(option => option === selectedOption);
-        
+
             if (e.key === "ArrowDown" || e.key === "ArrowRight") {
                 e.preventDefault();
                 if (currentIndex < options.length - 1) {
                     const next = currentIndex + 1;
                     setSelectedOption(options[next]);
                     onChange?.(options[next].value);
-                }  
+                }
                 return;
             }
-        
+
             if (e.key === "ArrowUp" || e.key === "ArrowLeft") {
                 e.preventDefault();
                 if (currentIndex > 0) {
@@ -55,46 +70,68 @@ function Dropdown({ options, onChange, icon, disabled = false }) {
                 }
                 return;
             }
+
+            if (e.key === "Home" || e.key === "PageUp") {
+                e.preventDefault();
+                setSelectedOption(options[0]);
+                onChange?.(options[0].value);
+                return;
+            }
+
+            if (e.key === "End" || e.key === "PageDown") {
+                e.preventDefault();
+                const lastIndex = options.length - 1;
+                setSelectedOption(options[lastIndex]);
+                onChange?.(options[lastIndex].value);
+                return;
+            }
         }
 
         if (isOpen) {
-        if (e.key === "ArrowDown" || e.key === "ArrowRight") {
-            e.preventDefault(); 
-            //Si prev n'est pas le dernier élément de la liste, on l'incrémente de 1 sinon on le met à la dernière option de la liste
-            setFocusedList(prev => {
-                const next = prev < options.length - 1 ? prev + 1 : options.length - 1;
-                // Prends la référence du prochain élément (next) dans mon tableau de refs (liRefs.current) 
-                // et fais en sorte qu’il devienne visible automatiquement en scrollant juste ce qu'il faut
-                liRefs.current[next]?.scrollIntoView({ block: "nearest" });
-                return next;
-            });
-        }
+            if (e.key === "ArrowDown" || e.key === "ArrowRight") {
+                e.preventDefault();
+                //Si prev n'est pas le dernier élément de la liste, on l'incrémente de 1 sinon on le met à la dernière option de la liste
+                setFocusedList(prev => {
+                    const next = prev < options.length - 1 ? prev + 1 : options.length - 1;
+                    return next;
+                });
+            }
             // Si prev n'est pas le premier élément de la liste, on le décrémente de 1 sinon on le met à 0
-        if (e.key === "ArrowUp" || e.key === "ArrowLeft") {
-            e.preventDefault();
-            setFocusedList(prev => {
-                const next = prev > 0 ? prev - 1 : 0;
-                liRefs.current[next]?.scrollIntoView({ block: "nearest" });
-                return next;
-            });
-        }
+            if (e.key === "ArrowUp" || e.key === "ArrowLeft") {
+                e.preventDefault();
+                setFocusedList(prev => {
+                    const next = prev > 0 ? prev - 1 : 0;
+                    return next;
+                });
+            }
 
-        if (e.key === "Enter" || e.key === " ") {         
-            e.preventDefault()
-            if (!isOpen) { // déclenche si le menu est fermé
-                setIsOpen(true); // ouvre le menu
-                setFocusedList(0); // met le focus sur la première option
+            if (e.key === "Enter" || e.key === " ") {
+                e.preventDefault()
+                if (!isOpen) { // déclenche si le menu est fermé
+                    setIsOpen(true); // ouvre le menu
+                    const index = options.findIndex(option => option === selectedOption);
+                    setFocusedList(index); // focus l'option sélectionnée 
+                }
+                else if (focusedList >= 0) {
+                    onSelect(options[focusedList]);//permet de sélectionner l'option focusée par le clavier
+                }
             }
-            else if (focusedList >= 0) { 
-                onSelect(options[focusedList]);//permet de sélectionner l'option focusée par le clavier
+
+            if (e.key === "Escape") {
+                setIsOpen(false);
+            }
+
+            if (e.key === "Home" || e.key === "PageUp") {
+                e.preventDefault();
+                setFocusedList(0);
+            }
+            
+            if (e.key === "End" || e.key === "PageDown") {
+                e.preventDefault();
+                setFocusedList(options.length - 1);
             }
         }
- 
-        if (e.key === "Escape") {
-            setIsOpen(false);
-        }
-    }
-};
+    };
 
     useEffect(() => {
         const outClickCloser = (e) => {
@@ -102,19 +139,26 @@ function Dropdown({ options, onChange, icon, disabled = false }) {
                 setIsOpen(false);
             }
         };
-    
         document.addEventListener("mousedown", outClickCloser);
-    
+
         return () => {
             document.removeEventListener("mousedown", outClickCloser);
         };
     }, [isOpen]);
 
+
+    // useEffect pour gérer le focus sur l'élément de la liste à l'ouverture du menu
+    useEffect(() => {
+            if (isOpen && typeof focusedList === "number") {
+            liRefs.current[focusedList]?.scrollIntoView({ block: "nearest" });
+        }
+    }, [isOpen, focusedList]);
+
     return (
-        
+
         <div className="jsWrapper">
             <div className="jsDropdown"
-                onKeyDown={handleKeyDown}                
+                onKeyDown={handleKeyDown}
             >
                 <button className={`jsDropdownButton ${isOpen ? "open" : ""}`}
                     onClick={Toggle}
@@ -129,8 +173,8 @@ function Dropdown({ options, onChange, icon, disabled = false }) {
                             <li
                                 key={index}
                                 ref={(el) => (liRefs.current[index] = el)} // on ajoute une référence à chaque élément de la liste
-                                onClick={() => onSelect(option)}                               
-                                className={index === focusedList ? "focused" : ""}                                
+                                onClick={() => onSelect(option)}
+                                className={index === focusedList ? "focused" : ""}
                             >
                                 {option.label}
                             </li>
